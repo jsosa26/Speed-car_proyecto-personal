d720680fd3e02dec0fe29ffff12890fdba4e20f0
jsosa26 <jesus.asm26@gmail.com>
2023-01-31 01:57:35 +0000

    img and more edit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import instagram from "../../img/instagram.png"
+import facebook from "../../img/facebook.png"
+import github from "../../img/silueta-del-logo-de-github-en-un-cuadrado.png"
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -13,7 +16,12 @@ return (
           <span >Speed Test</span>
 		  <span >Somos una compañia con crecimiento exponencial en el mercado Automotriz, ofreciendo las mejores cuotas a nuestros clientes, y maxima seguridad a sus compradores</span>
         </div>
-		<button className="button i-button">Nuestro Catalogo</button>
+		<button className="button i-button">Catálogo</button>
+		<div className="i-icon">
+			<img src={instagram}></img>
+			<img src={facebook}></img>
+			<img src={github}></img>
+		</div>
       </div>
       <div className="i-right"> jggnjsgnsglsdgh</div>
     </div>
