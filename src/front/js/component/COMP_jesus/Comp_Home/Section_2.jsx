d313b97676e3f2dd459079360a5f6b2183e41f2e
jsosa26 <jesus.asm26@gmail.com>
2023-02-25 14:51:43 +0000

    nuevamente segunta parte del home

---
+++ src/front/js/component/COMP_jesus/Comp_Home/Section_2.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "../Comp_Home/Section_2.css"
+import glasses from "../../img/glasses.png";
+import heartemoji from "../../img/heartemoji.png";
+import humble from "../../img/humble.png";
+
+export const Navbar = () => {
+
+    return (
+        <div className="Servicios">
+            {/* Lado Izquierdo */}
+            <div className="awesome">
+                <span>Nuestros Servicios</span>
+                <span>Son los Mejores</span>
+                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum eos ullam rerum accusantium labore tenetur ex iusto, suscipit quaerat eaque eum commodi deleniti enim soluta aliquid nobis ipsum pariatur doloribus!</span>
+            </div>
+            {/* Lado Derecho */}
+            <div className="Cards">
+
+            </div>
+        </div>
+    )
+}
