031d6daeb703da290fc4dbdb0c07d9a8ab556f71
jsosa26 <jesus.asm26@gmail.com>
2023-03-08 16:48:51 +0000

    change img format

--- src/front/js/component/CompJesus/CompHome/Section4.jsx
+++ src/front/js/component/CompJesus/CompHome/Section4.jsx
@@ -1,42 +1,44 @@
-import React from "react"
+import React from "react";
 import "../../../../styles/section4.css";
-import toyot from "../../../../img/chevrolet.css";
-import mitsubishi from "../../../../img/ford.css";
+import toyot from "../../../../img/chevrolet.png";
+import mitsubishi from "../../../../img/ford.png";
 // import ford from "../../../../img/mitsubishi.css";
-import chevrolet from "../../../../toyot.css";
-import chevrolet from "../../../../img/honda.css";
+import chevrolet from "../../../../toyot.png";
+import chevrolet from "../../../../img/honda.png";
 
 export const Section4 = () => {
-
-    return (
-        <div className="section4">
-            {/* Left Side */}
-            <div className="awesome">
-                <span>Nuestras Mercas</span>
-                <span>Más Vendidas</span>
-                <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br/> Nostrum eos ullam rerum accusantium labore tenetur ex iusto,<br/> suscipit quaerat eaque
-                eum commodi deleniti enim soluta aliquid <br/> nobis ipsum pariatur doloribus!</span>
-                <button className="button s-button">Catálogo</button>
-                <div className="blur s-blurl" style={{ background: "#ABF1FF94"}}></div>
-            </div>
-            {/* Right Side */}
-            <div className="w-right">
-                <div className="mainCircle">
-                    <div className="w-secCircle">
-                        <img src={chevrolet} alt="" />
-                    </div>
-                    <div className="w-secCircle">
-                        <img src={honda} alt="" />
-                    </div>
-                    <div className="w-secCircle">
-                        <img src={toyot} alt="" />
-                    </div>
-                    <div className="w-secCircle">
-                        <img src={ford} alt="" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="section4">
+      {/* Left Side */}
+      <div className="awesome">
+        <span>Nuestras Mercas</span>
+        <span>Más Vendidas</span>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          <br /> Nostrum eos ullam rerum accusantium labore tenetur ex iusto,
+          <br /> suscipit quaerat eaque eum commodi deleniti enim soluta aliquid{" "}
+          <br /> nobis ipsum pariatur doloribus!
+        </span>
+        <button className="button s-button">Catálogo</button>
+        <div className="blur s-blurl" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* Right Side */}
+      <div className="w-right">
+        <div className="mainCircle">
+          <div className="w-secCircle">
+            <img src={chevrolet} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={honda} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={toyot} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={ford} alt="" />
+          </div>
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
