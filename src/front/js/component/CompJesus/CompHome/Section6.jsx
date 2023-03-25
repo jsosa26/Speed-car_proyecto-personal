6de0dcb8feb2bfa51c57b45748984e80e706fc07
jsosa26 <jesus.asm26@gmail.com>
2023-03-25 23:58:38 +0000

    errores en la seccion 6

--- src/front/js/component/CompJesus/CompHome/Section6.jsx
+++ src/front/js/component/CompJesus/CompHome/Section6.jsx
@@ -12,6 +12,30 @@ export const Section6 = () => {
                 <div className="blur t-blur2" style={{ background: "skyblue"}}></div>
             </div>
             {/* slider */}
+            <div class="card">
+                <img src="..." class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" aria-hidden="true">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title placeholder-glow">
+                    <span class="placeholder col-6"></span>
+                    </h5>
+                    <p class="card-text placeholder-glow">
+                        <span class="placeholder col-7"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-4"></span>
+                        <span class="placeholder col-6"></span>
+                        <span class="placeholder col-8"></span>
+                    </p>
+                    <a class="btn btn-primary disabled placeholder col-6"></a>
+                </div>
+            </div>
         </div>
     );
 };
